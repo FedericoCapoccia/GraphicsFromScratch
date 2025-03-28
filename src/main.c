d08ba7a88172f18b868e7a2015eb85848e1f991c
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-03-28 18:01:20 +0100

    added vulkan stuff to vcpkg (still need validation layers on system)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
+#define VOLK_IMPLEMENTATION
+
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <volk.h>
+#include <vulkan/vk_enum_string_helper.h>
 
 int main()
 {
@@ -20,9 +23,33 @@ int main()
         return -3;
     }
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    VkApplicationInfo app_info = {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Engine",
+        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "GraphicsFromScratch",
+        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = VK_API_VERSION_1_4
+    };
+
+    const char* layers[] = { "VK_LAYER_KHRONOS_validation" };
+
+    VkInstanceCreateInfo create_info = {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &app_info,
+        .enabledLayerCount = 1,
+        .ppEnabledLayerNames = layers
+    };
+
+    VkInstance instance;
+    VkResult res = vkCreateInstance(&create_info, nullptr, &instance);
+    if (res != VK_SUCCESS) {
+        printf("Failed to create VkInstance: {%s}\n", string_VkResult(res));
     }
+
+    // while (!glfwWindowShouldClose(window)) {
+    //     glfwPollEvents();
+    // }
 
     return 0;
 }
