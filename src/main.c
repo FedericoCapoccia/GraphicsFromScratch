a107e8aa82d19ec54c6889231ed0ea7ae7bf5edd
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-03-29 13:29:47 +0100

    trying to do something right!

--- src/main.c
+++ src/main.c
@@ -13,11 +13,17 @@ int main(void)
         return -1;
     }
 
-    window_t window;
-    window_initialize(&window, 800, 600, "Engine");
+    window_t window = { 0 };
+    if (!window_initialize(&window, 800, 600, "Engine")) {
+        printf("Failed to initialize window\n");
+        return -2;
+    }
 
-    renderer_t renderer;
-    renderer_initialize(&renderer, &window, true);
+    renderer_t renderer = { 0 };
+    if (!renderer_initialize(&renderer, &window, true)) {
+        printf("Failed to initialize renderer\n");
+        return -3;
+    }
 
     while (!window_should_close(&window)) {
         window_poll_events();
